dbe5888a8c3e92ea008d45eb510bb1a30d425650
Daniel Santos <tkdsantos@gmail.com>
2021-03-20 21:28:14 -0300

    chore: Commit inicial

---
+++ index.js
@@ -0,0 +1,10 @@
+const { response } = require("express")
+const express = require("express")
+
+const app = express()
+
+app.get("/teste", (req, res) => {
+  return  res.json({ Hello: "World"})
+})
+
+app.listen(3333)
