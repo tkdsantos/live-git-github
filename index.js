eb66c443f8cae3f5e9b4c4de5a5e6fd3129e6970
Daniel Teixeira dos Santos <tkdsantos@gmail.com>
2021-03-21 09:10:51 -0300

    Revert "chore: courses-pages"

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express")
 const app = express()
 
 app.get("/teste", (req, res) => {
-  return  res.json({ Hello: "COURSES-PAGES"})
+  return  res.json({ Hello: "World 2"})
 })
 
 app.listen(3333)
